1301515026c377d65585796477422638235972fd
Yush Kapoor <YushDotKapoor@gmail.com>
2024-12-15 03:59:49 -0500

    made service functions "private"

--- service_config_foundry/service.py
+++ service_config_foundry/service.py
@@ -29,58 +29,26 @@ class Service:
         self.scope_file = FileType.SCOPE
 
     # Returns the full path for a specific configuration file.
-    def get_path(self, file):
+    def __get_path(self, file):
         return self.service_location.directory() + "/" + file.file_name(self.name)
     
     # Yields the configurations of all file types, optionally checking requirements.
-    def file_configs(self, requirement_check=True):
+    def __file_configs(self, requirement_check=True):
         for file in [self.service_file, self.socket_file, self.mount_file, self.automount_file, self.swap_file, self.path_file, self.timer_file]:
             config = file.get_config(requirement_check=requirement_check)
             if config:
                 yield file, config
     
     # Checks if any files for the service name already exist in the directory.
-    def service_with_name_exists(self):
+    def __service_with_name_exists(self):
         files = []
         for file in os.listdir(self.service_location.directory()):
             if file.startswith(f"{self.name}."):
                 files.append(file)
         return files
-
-    # Creates the service configuration files.
-    def create(self):
-        if self.service_with_name_exists() and not self.force_overwrite:
-            raise ValueError(f"Service for {self.name} already exists")
-        self.replace()
-
-    # Deletes all files associated with the service name.
-    def delete(self):
-        for file in os.listdir(self.service_location.directory()):
-            if file.startswith(f"{self.name}."):
-                try:
-                    os.remove(os.path.join(self.service_location.directory(), file))
-                except PermissionError:
-                    print(f"Permission denied: cannot write to {file}. Try running as root or using sudo.")
-                    raise
-
-    # Replaces the existing service configuration files with new ones.
-    def replace(self):
-        config_and_path = {}
-        # Prepare configurations and paths for atomic replacement.
-        for file, config in self.file_configs():
-            path = self.get_path(file)
-            config_and_path[path] = config
-
-        # Delete old configurations before writing new ones.
-        self.delete()
-
-        # Write the new configurations to their respective files.
-        for path, config in config_and_path.items():
-            with open(path, "w") as f:
-                config.write(f)
     
     # Adds attributes to a file based on the given configuration.
-    def add_attributes(self, file, config):
+    def __add_attributes(self, file, config):
         file_tag = file.split(".")[-1]
         for section, unit in config.items():
             for key, value in unit.items():
@@ -105,6 +73,38 @@ class Service:
                 section_obj = getattr(file_type, section.lower(), None)
                 if section_obj:
                     setattr(section_obj, convert_to_snake_case(key), value)
+
+    # Creates the service configuration files.
+    def create(self):
+        if self.__service_with_name_exists() and not self.force_overwrite:
+            raise ValueError(f"Service for {self.name} already exists")
+        self.replace()
+
+    # Deletes all files associated with the service name.
+    def delete(self):
+        for file in os.listdir(self.service_location.directory()):
+            if file.startswith(f"{self.name}."):
+                try:
+                    os.remove(os.path.join(self.service_location.directory(), file))
+                except PermissionError:
+                    print(f"Permission denied: cannot write to {file}. Try running as root or using sudo.")
+                    raise
+
+    # Replaces the existing service configuration files with new ones.
+    def replace(self):
+        config_and_path = {}
+        # Prepare configurations and paths for atomic replacement.
+        for file, config in self.__file_configs():
+            path = self.get_path(file)
+            config_and_path[path] = config
+
+        # Delete old configurations before writing new ones.
+        self.delete()
+
+        # Write the new configurations to their respective files.
+        for path, config in config_and_path.items():
+            with open(path, "w") as f:
+                config.write(f)
         
     # Updates the service with new configurations.
     def update(self):
@@ -121,20 +121,20 @@ class Service:
 
         # Collect current and new configurations.
         config_and_path = {}
-        for file, config in self.file_configs(requirement_check=False):
-            path = self.get_path(file)
+        for file, config in self.__file_configs(requirement_check=False):
+            path = self.__get_path(file)
             config_and_path[path] = config._sections
         
         # Add attributes from relevant files to the temporary service.
         for file in relevant_files:
             config = CaseSensitiveConfigParser()
             config.read(os.path.join(temp_service.service_location.directory(), file))
-            temp_service.add_attributes(file, config)
+            temp_service.__add_attributes(file, config)
 
         # Merge the original and new configurations.
         original_config_and_path = {}
-        for file, config in temp_service.file_configs(requirement_check=False):
-            path = temp_service.get_path(file)
+        for file, config in temp_service.__file_configs(requirement_check=False):
+            path = temp_service.__get_path(file)
             original_config_and_path[path] = config._sections
         
         final_config_and_path = {}
@@ -146,6 +146,6 @@ class Service:
         # Update attributes and replace the configuration files.
         for path, _ in final_config_and_path.items():
             file = os.path.basename(path)
-            self.add_attributes(file, final_config_and_path[path])
+            self.__add_attributes(file, final_config_and_path[path])
 
         self.replace()
